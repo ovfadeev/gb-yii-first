30cff7a3ad3c6605182e61ad96a1e96b967aa972
o.fadeev <o.fadeev@live.ru>
2018-06-04 22:10:44 +0300

    hw3

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -3,12 +3,18 @@
 namespace app\controllers;
 
 use yii\web\controller;
-use app\models\repository\Users;
+use app\models\repository\Tasks;
 
 class TaskController extends Controller
 {
   public function actionIndex()
   {
-    return $this->render('index', array("title" => "Урок 1", "description" => "Контроллер Task"));
+    return $this->render('index', ["title" => "Урок 1", "description" => "Контроллер Task"]);
+  }
+
+  public function actionCreate()
+  {
+    $tasks = new Tasks();
+    return $this->render('create', ['model' => $tasks]);
   }
 }
