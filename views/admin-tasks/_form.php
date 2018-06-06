082c9fe9b1cc26eee7f7174c978db28187d825b1
ofadeev <ofadeev@maxlevel.ru>
2018-06-06 14:41:36 +0300

    hw3

---
+++ views/admin-tasks/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\repository\Tasks */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="tasks-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'deadline')->textInput(['type' => 'date']) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'autor_id')->textInput() ?>
+
+    <?= $form->field($model, 'performer_id')->textInput() ?>
+
+    <?= $form->field($model, 'status_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
