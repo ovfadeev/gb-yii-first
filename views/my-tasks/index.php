70a15a7f76a92df2d7c643750639ad0483d107c7
ofadeev <ofadeev@maxlevel.ru>
2018-06-07 14:07:57 +0300

    hw4 ticket 1

--- views/my-tasks/index.php
+++ views/my-tasks/index.php
@@ -10,56 +10,66 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="my-tasks-index">
 
   <h1><?= Html::encode($this->title) ?></h1>
-  <div class="row">
-    <?php
-    foreach ($calendar as $day => $model) {
-      echo '<div class="col-lg-1 alert-success">' . $day . '</div>';
-      echo '<div class="col-lg-11">';
-      $dataProvider = new ActiveDataProvider([
-          'query' => $model
-      ]);
-      echo GridView::widget([
-          'dataProvider' => $dataProvider,
-          'filterModel' => $searchModel,
-          'columns' => [
-              ['class' => 'yii\grid\SerialColumn'],
+    <table class="table table-bordered">
+      <thead>
+      <tr>
+        <th class="bg-success">Day</th>
+        <th>Tasks</th>
+      </tr>
+      </thead>
+      <tbody>
+        <?php
+        foreach ($calendar as $day => $model) {
+          echo '<tr>';
+          echo '<th class="bg-success">' . $day . '</th>';
+          echo '<th>';
+          $dataProvider = new ActiveDataProvider([
+              'query' => $model
+          ]);
+          echo GridView::widget([
+              'dataProvider' => $dataProvider,
+              'filterModel' => $searchModel,
+              'columns' => [
+                  ['class' => 'yii\grid\SerialColumn'],
 
-              'id',
-              'name',
-              'deadline',
-              'description:ntext',
-              'autor_id' => [
-                  'attribute' => 'autor_id',
-                  'value' => function ($model) {
-                    $user = $model->getAutor()->where(['id' => $model->autor_id])->one();
-                    return $user->getFullName();
-                  }
-              ],
-              'performer_id' => [
-                  'attribute' => 'performer_id',
-                  'value' => function ($model) {
-                    $user = $model->getPerformer()->where(['id' => $model->performer_id])->one();
-                    return $user->getFullName();
-                  }
-              ],
-              'status_id' => [
-                  'attribute' => 'status_id',
-                  'value' => function ($model) {
-                    $status = $model->getStatus()->where(['id' => $model->status_id])->one();
-                    return $status->title;
-                  }
-              ],
+                  'id',
+                  'name',
+                  'deadline',
+                  'description:ntext',
+                  'autor_id' => [
+                      'attribute' => 'autor_id',
+                      'value' => function ($model) {
+                        $user = $model->getAutor()->where(['id' => $model->autor_id])->one();
+                        return $user->getFullName();
+                      }
+                  ],
+                  'performer_id' => [
+                      'attribute' => 'performer_id',
+                      'value' => function ($model) {
+                        $user = $model->getPerformer()->where(['id' => $model->performer_id])->one();
+                        return $user->getFullName();
+                      }
+                  ],
+                  'status_id' => [
+                      'attribute' => 'status_id',
+                      'value' => function ($model) {
+                        $status = $model->getStatus()->where(['id' => $model->status_id])->one();
+                        return $status->title;
+                      }
+                  ],
 
-              [
-                  'class' => 'yii\grid\ActionColumn',
-                  'visibleButtons' => [
-                      'delete' => false
-                  ]
+                  [
+                      'class' => 'yii\grid\ActionColumn',
+                      'visibleButtons' => [
+                          'delete' => false
+                      ]
+                  ],
               ],
-          ],
-      ]);
-      echo '</div>';
-    }
-    ?>
-  </div>
+          ]);
+          echo '</th>';
+          echo '</tr>';
+        }
+        ?>
+      </tbody>
+    </table>
 </div>
