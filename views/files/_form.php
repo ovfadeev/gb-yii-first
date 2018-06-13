3dab8dbbd60084a3779d82859fa1c7d9b2d5003a
ofadeev <ofadeev@maxlevel.ru>
2018-06-13 10:46:10 +0300

    hw6

--- views/files/_form.php
+++ views/files/_form.php
@@ -16,10 +16,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'path')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'date_create')->textInput() ?>
-
-    <?= $form->field($model, 'date_update')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
