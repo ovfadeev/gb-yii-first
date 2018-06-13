f12a2dcd2b82a5de1704b8f66b2ee05235d88ab4
ofadeev <ofadeev@maxlevel.ru>
2018-06-13 10:05:34 +0300

    hw6

--- migrations/m180611_183120_create_files_table.php
+++ migrations/m180611_183120_create_files_table.php
@@ -14,6 +14,7 @@ class m180611_183120_create_files_table extends Migration
   {
     $this->createTable('files', [
         'id' => $this->primaryKey(),
+        'title' => $this->string(50)->notNull(),
         'path' => $this->string(150)->notNull(),
         'date_create' => 'DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL',
         'date_update' => 'DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL',
